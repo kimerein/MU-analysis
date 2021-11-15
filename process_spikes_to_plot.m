0ab11a2a9049e7f67c6b08667b2e3c846848e957
kimerein <kimerein@users.noreply.github.com>
2021-11-15 15:33:36 -0500

    convert spikes to tbt from WHISPER rec system

--- process_spikes_to_plot.m
+++ process_spikes_to_plot.m
@@ -5,8 +5,10 @@ function [spikes,cuetimes,optotimes]=process_spikes_to_plot(filename,spikes)
 % then switches spikes structure from continuous form to trial-by-trial
 
 % filename='\\research.files.med.harvard.edu\neurobio\MICROSCOPE\Kim\Behavior on Electrophys Rig\str_white\20180417 SORT THIS\str.pl2';
-cuech='AI01';
-ledch='AI03';
+cuech='AI01'; % Plexon system
+ledch='AI03'; % Plexon system
+ledch='auxData193'; % WHISPER system distractor LED
+cuech='auxData195invert'; % WHISPER system inverted cue
 
 % convert spikes to cue-triggered trial-by-trial
 [spikes,cuetimes,optotimes]=convertToTrialByTrial_wrapper(spikes,filename,cuech,ledch);
